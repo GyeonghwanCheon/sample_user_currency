7b9e8100666d86baea750638bf4606e5174ec1e5
myoma <qq8380qq@gmail.com>
2024-11-27 17:29:43 +0900

    feat : init commit

--- src/main/java/com/sparta/currency_user/controller/UserController.java
+++ src/main/java/com/sparta/currency_user/controller/UserController.java
@@ -16,21 +16,25 @@ public class UserController {
 
     private final UserService userService;
 
+    // 유저 조회
     @GetMapping
     public ResponseEntity<List<UserResponseDto>> findUsers() {
         return ResponseEntity.ok().body(userService.findAll());
     }
 
+    // 특정 유저 조회
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDto> findUser(@PathVariable Long id) {
         return ResponseEntity.ok().body(userService.findById(id));
     }
 
+    // 유저 생성
     @PostMapping
     public ResponseEntity<UserResponseDto> createUser(@RequestBody UserRequestDto userRequestDto) {
         return ResponseEntity.ok().body(userService.save(userRequestDto));
     }
 
+    // 유저 삭제
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable Long id) {
         userService.deleteUserById(id);
